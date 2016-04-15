0cacf5da72ba5e515a4a8bc8653333251f38b92e
Dominick Malzone <dominickmalzone@aol.com>
2016-04-15 08:14:09 +0000

    fixed error

--- hallcam/tempimage.py
+++ hallcam/tempimage.py
@@ -4,7 +4,7 @@ import os
 class TempImage:
     def __init__(self, basePath="./", ext=".jpg"):
     #construct file path
-        self.path = "{base_path}/{rand}{ext}".format(base_path=BasePath,
+        self.path = "{base_path}/{rand}{ext}".format(base_path=basePath,
                                                      rand=str(uuid.uuid4()), ext=ext)
 
     def cleanup(self):
