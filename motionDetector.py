0991ca9727e5647e86cec0d111e8b85c345e2974
Dominick Malzone <dominickmalzone@aol.com>
2016-04-15 06:44:47 +0000

    dynamically adjust to background changes and dropbox

--- motionDetector.py
+++ motionDetector.py
@@ -1,7 +1,7 @@
 from hallcam.tempimage import TempImage
 from dropbox.client import DropboxOAuth2FlowNoRedirect
 from dropbox.client import DropboxClient
-#from picamera.array import PiRGBArray
+from picamera.array import PiRGBArray
 #from picamera import PiCamera
 import argparse
 import warnings
@@ -10,6 +10,7 @@ import imutils
 import json
 import time
 import cv2
+
 
 #construct the argument parser and parse arguments
 ap = argparse.ArgumentParser()
@@ -22,6 +23,8 @@ warnings.filterwarnings("ignore")
 conf = json.load(open(args["conf"]))
 client = None
 
+
+"""
 if conf["use_dropbox"]:
     #connect to dropbox and start the session auth process
     flow = DropboxOAuth2FlowNoRedirect(conf["dropbox_key"], conf["dropbox_secret"])
@@ -32,64 +35,108 @@ if conf["use_dropbox"]:
     (accessToken, userID) = flow.finish(authCode)
     client = DropboxClient(accessToken)
     print "[SUCCESS] dropbox account linked"
+"""
     
-#webcam yo, 0= use first cam detected 1=second cam detected
+#grab cam, set height width and fps
 cam = cv2.VideoCapture(0)
-time.sleep(0.25)
+cam.set(3,640)
+cam.set(4,480)
+cam.set(5,16)#frames
+rawCapture = PiRGBArray(cam, size=tuple(conf["resolution"]))
 
-#initialize first frame, assuming contains no motion and just background
-firstFrame= None
+                        
 
-#loop over frames of video
+#cam warm up, then initialize average frame, timestamp and and frame motion cntr
+time.sleep(conf["camera_warmup_time"])
+avg = None
+lastUploaded = datetime.datetime.now()
+motionCounter = 0
+ 
+#capture frames from the camera
 while True:
-    #grab current frame and set occupied or unoccupied
-    (grabbed, frame) = cam.read()
-    text = 'Unnoccupied'
+    (grabbed, frame) = cam.read()   
+    #grab the raw numpy array representing image and initialize it    
+    timestamp = datetime.datetime.now()
+    text = "Unnoccupied"
 
-    #if the frame could not be grabbed, then vid end
-    if not grabbed:
-        break
     #resize frame, grayscale and blurrrr!
     frame = imutils.resize(frame, width=500)
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
     gray = cv2.GaussianBlur(gray, (21,21), 0)
 
-    if firstFrame is None:
-        firstFrame = gray
+    #if the average frame is None, initialize it
+    if avg is None:
+        print "[INFO] starting background model.."
+        avg = gray.copy().astype("float")
+        rawCapture.truncate(0)
         continue
-    #compute absolute difference between current and first frame
-    frameDelta = cv2.absdiff(firstFrame, gray)
-    thresh = cv2.threshold(frameDelta, 25, 255, cv2.THRESH_BINARY)[1]
 
-    #dilate the thresholded image to fill holes, then find contours on it
+    #accumuulate the weighted average between the current and previous frame
+    #then compute differene between current frame and running average    
+    cv2.accumulateWeighted(gray, avg, 0.5)
+    frameDelta = cv2.absdiff(gray, cv2.convertScaleAbs(avg))
+
+    #threshold the delta image, dilate the thresholded img to fill holes
+    #then find contours on thresholded img
+    thresh = cv2.threshold(frameDelta, conf["delta_thresh"],255,
+                           cv2.THRESH_BINARY)[1]
     thresh = cv2.dilate(thresh, None, iterations=2)
     (cnts, _) = cv2.findContours(thresh.copy(), cv2.RETR_EXTERNAL,
                                  cv2.CHAIN_APPROX_SIMPLE)
-    #loop over the contours and ignore small ones
-    for c in cnts:
-        if cv2.contourArea(c) < args['min_area']:
+    #loop over the contours
+    for c in cnts:        
+        #if the contour is too small, ignore it
+        if cv2.contourArea(c) < conf["min_area"]:
             continue
-        #compute the bounding box for the countour and draw it on frame
-        (x,y,w,h) = cv2.boundingRect(c)
-        cv2.rectangle(frame, (x,y), (x + w, y + h), (0, 255,0),2)
-        text = 'Occupied'
-        #draw text and timestamp
-        cv2.putText(frame, 'Room status:{}'.format(text),(10,20),
+        #compute the bounding box for contour and draw it, along with text.
+        (x, y, w, h) = cv2.boundingRect(c)
+        cv2.rectangle(frame, (x,y),(x + w, y + h), (0,255,0),2)
+        text = "Occupied"
+
+        #draw the text and timestamp on the frame
+        ts = timestamp.strftime('%A %d %B %Y %I:%M:%S%p')
+        cv2.putText(frame, "Room Status: {}".format(text),(10,20),
                     cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0,0,255),2)
-        cv2.putText(frame, datetime.datetime.now().strftime('%A %d %B %Y %I:%M:%S%p'),
-                    (10, frame.shape[0] - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.35, (0,0,255),1)
+        cv2.putText(frame, ts,(10, frame.shape[0] - 10), cv2.FONT_HERSHEY_SIMPLEX,
+                    0.35,(0,0,255),1)
 
-        #show the frame and record if the user presses a key
-        cv2.imshow('Security feed', frame)
-        cv2.imshow('Thersh', thresh)
-        cv2.imshow('Frame Delta', frameDelta)
-        key = cv2.waitKey(1) & 0xFF
+        #check to see if occupied
+        if text == "Occupied":
+            #check to see if enough time has past between uploads
+            if (timestamp - lastUploaded).seconds >= conf["min_upload_seconds"]:
+                motionCounter += 1
 
-        if key == ord("q"):
-            break
-        
-        
-        
-        
+                #check to see if the number of frames with consistent motion is high enough
+                if motionCounter >= conf["min_motion_frames"]:
+                    if conf["use_dropbox"]:
+                        t = TempImage()
+                        cv2.imwrite(t.path, frame)
+
+                        #upload image to dropbox and cleanup temp image
+                        print "[UPLOAD] {}".format(ts)
+                        path = "{base_path}/{timestamp}.jpg".format(
+                            base_path=conf["drop_box_path"], timestamp=ts)
+                        clinet.put_file(path, open(t.path, "rb"))
+                        t.cleanup()
+                    #update the last uploaded timestamp and reset motion counter
+                        lastUploaded = timestamp
+                        motionCounter = 0
+        #otherwise room not occupied
+        else:
+            motionCounter = 0
+        if conf["show_video"]:
+            #display the security feed
+            cv2.imshow('Security Feed', frame)
+            key = cv2.waitKey(1) & 0xFF
+
+            # if the q is pressed, break
+            if key == ord('q'):
+                break
+        #clear teh stream in preparation for the next frame
+        rawCapture.truncate(0)
+    
+
+
 camera.release()
 cv2.destroyAllWindows()
+
